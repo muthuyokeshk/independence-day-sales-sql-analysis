a8812bc6900397920a03061baf2f904ee2c20d57
Muthu Yokesh K <muthuyokesh.k2005@gmail.com>
2026-08-13 15:35:13 +0200

    Update 02_aggregate_functions.sql

--- queries/02_aggregate_functions.sql
+++ queries/02_aggregate_functions.sql
@@ -1,1 +1,16 @@
+-- Independence Day Sales Analysis
+-- Aggregate SQL Queries
 
+
+--Q1. Find the total sales amount.
+
+
+--Q2. Find the average sales amount.
+--Q3. Find the maximum sales amount.
+--Q4. Find the minimum sales amount.
+--Q5. Find the total quantity sold.
+--Q6. Count the total number of sales transactions.
+--Q7. Find the total sales amount for the Electronics category.
+--Q8. Find the average quantity sold per transaction.
+--Q9. Find the total sales for Tamil Nadu.
+--Q10. Find the number of unique customers.
