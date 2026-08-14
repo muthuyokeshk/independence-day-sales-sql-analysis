d20163b010ba636f6917cbc961d5547c92003026
Muthu Yokesh K <muthuyokesh.k2005@gmail.com>
2026-08-14 09:19:45 +0200

    Update 04_date_analysis.sql

--- queries/04_date_analysis.sql
+++ queries/04_date_analysis.sql
@@ -1,1 +1,13 @@
 
+
+
+1. Find the total sales made during January 2026.
+2. Find the total sales made during Independence Day month – August 2026.
+3. Display sales made in the last 30 days.
+4. Find total sales for each month.
+5. Find the number of transactions for each month.
+6. Find the highest-sales month.
+7. Find the lowest-sales month.
+8. Find total sales for each year.
+9. Find sales made on weekends.
+10. Find the number of sales transactions made on each day of the week.
